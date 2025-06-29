e6edf9a951237bebd54edf0c7fe16a589a5019d2
Andrew Wittrock <andy@wittrock.us>
2025-06-29 16:05:50 -0500

    Add subscription plan types

---
+++ types.d.ts
@@ -0,0 +1,11 @@
+export declare interface ISubscriptionPlan {
+    id: string;
+    name: string;
+    planId: string;
+    description: string;
+    renews: string;
+    period: number; // in months
+    price: number; // in USD
+}
+
+export type NewSubscriptionPlan = Omit<ISubscriptionPlan, "id">;
